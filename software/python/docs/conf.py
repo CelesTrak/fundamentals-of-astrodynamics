1fccb374bf54b1825008248df03dec6b9aa0f93a
daluar <dala.dev@zurdala.es>
2026-05-31 01:22:54 +0200

    Update conf.py to 2026

--- software/python/docs/conf.py
+++ software/python/docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "valladopy"
-copyright = "2025, D. A. Vallado and Contributors"
+copyright = "2026, D. A. Vallado and Contributors"
 author = "D. A. Vallado, maintained by S. Rolander"
 
 # -- General configuration ---------------------------------------------------
